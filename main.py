f6810420918003db1fa4d3e24e8d197050d95063
AO-XIN <aosiweixin@outlook.com>
2023-06-26 16:21:47 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import aug
 
-A=aug.aug("images","lables","images","lables")
-A.Rotate(11)
+A=aug.aug("images","lables","aug-images","aug-lables")# 原始图片路径 原始lables路径 扩充到的图片路径 扩充到的lables路径
+A.Rotate(80)
